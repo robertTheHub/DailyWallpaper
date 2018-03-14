9d8bb5900bb81722bd442061fa1fc9998f8c3112
Rob C <robert@robertjcoleman.com>
2018-03-13 18:50:52 -0700

    Made the subreddit modular with input via text field with hardcoded default

--- app/src/main/java/com/example/root/dailywallpaper/MainActivity.java
+++ app/src/main/java/com/example/root/dailywallpaper/MainActivity.java
@@ -7,8 +7,12 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -27,51 +31,9 @@ import java.net.HttpURLConnection;
 public class MainActivity extends AppCompatActivity {
     TextView mTextView;
     ImageView mView;
+    EditText mEditText;
+    Button mButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mTextView = (TextView) findViewById(R.id.text1);
-        mView = (ImageView) findViewById(R.id.imageView1);
-        String url = "https://www.reddit.com/r/iphonewallpapers/top/.json";
-
-        JsonObjectRequest jsonRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        // the response is already constructed as a JSONObject!
-                        try {
-                            JSONArray kids = response.getJSONObject("data").getJSONArray("children");
-                            int num_kids = kids.length();
-                            String[] urls = new String[num_kids];
-                            for(int i =0; i<num_kids; i++) {
-                                response = kids.getJSONObject(i).getJSONObject("data");
-                                if (response.getString("post_hint").equals("image")) {
-                                    urls[i] = response.getString("url");
-                                }
-                            }
-                            mTextView.setText(urls[0]);
-                            new DownloadImage().execute(urls);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener // Cannot resolve symbol ErrorListener
-                        () {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
-                    }
-                });
-
-        Volley.newRequestQueue(this).add(jsonRequest);
-
-
-
-
-    }
     // DownloadImage AsyncTask
     private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
 
@@ -95,9 +57,70 @@ public class MainActivity extends AppCompatActivity {
             try {
                 WallpaperManager.getInstance(getApplicationContext()).setBitmap(result);
             } catch (IOException e) {
-                // TODO Auto-generated catch block
+                // TODO Better Handle Errors
                 e.printStackTrace();
             }
+        }
     }
+
+    //Will get the top 25 posts from a subreddit, creating an array of urls
+    private void queryReddit(String subreddit){
+        subreddit = subreddit.replaceAll("\\s","");
+        if(subreddit.equalsIgnoreCase("")){
+            subreddit = "iphonewallpapers";
+        }
+        String url = "https://www.reddit.com/r/" + subreddit + "/top/.json";
+        JsonObjectRequest jsonRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            //Uses the response and checks if data is returned
+                            int num_kids = response.getJSONObject("data").getInt("dist");
+                            if (num_kids > 0){
+                                JSONArray kids = response.getJSONObject("data").getJSONArray("children");
+                                String[] urls = new String[num_kids];
+                                for(int i =0; i<num_kids; i++) {
+                                    response = kids.getJSONObject(i).getJSONObject("data");
+                                    if (response.getString("post_hint").equals("image")) {
+                                        urls[i] = response.getString("url");
+                                    }
+                                }
+                                mTextView.setText(urls[0]);
+                                new DownloadImage().execute(urls);
+                            } else {
+                                Toast.makeText(getApplicationContext(), R.string.subreddit_error, Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace(); //TODO Better handle errors
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(jsonRequest);
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.text1);
+        mView = (ImageView) findViewById(R.id.imageView1);
+        mEditText = (EditText) findViewById(R.id.editText);
+        mButton = findViewById(R.id.button);
+        queryReddit("");
+
+    }
+        public void buttonClick(View v) {
+            queryReddit(mEditText.getText().toString());
+        }
 }
+
+
+
